5d9be4841a7d2dbc2e672ad6f2feb8beeacae088
Mohamed <mohamed.e@trufla.com>
2019-04-07 09:44:33 +0200

    Added owners index controller closes #13

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class VetsController {
+public class VetController {
 
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
     public String listVets(){
