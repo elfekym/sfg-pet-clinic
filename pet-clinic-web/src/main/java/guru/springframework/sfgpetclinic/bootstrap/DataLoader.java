c024ef6d925a719b1d8a37db6be3469bc199d356
Mohamed <mohamed.e@trufla.com>
2019-04-14 21:31:19 +0200

    Refactoring Map Impl Base entity to generate ID, closes #23

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -21,14 +21,14 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
+        //owner1.setId(1L);
         owner1.setFirstName("Maria");
         owner1.setLastName("El-Feky");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
+        //owner2.setId(2L);
         owner2.setFirstName("Mo");
         owner2.setLastName("Salah");
 
@@ -37,13 +37,13 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners ....");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
+        //vet1.setId(1L);
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
+        //vet2.setId(2L);
         vet2.setFirstName("Zo");
         vetService.save(vet2);
         System.out.println("Loaded Vets ....");
