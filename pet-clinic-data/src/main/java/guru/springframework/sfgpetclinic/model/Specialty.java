2894c8ec57f4a36e8a25b4c37940eb6878b8ccae
Mohamed <mohamed.e@trufla.com>
2019-05-09 12:33:55 +0200

    updated Vets with JPA notations with related entities also, closes #37

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Specialty.java
@@ -1,5 +1,10 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialities")
 public class Specialty extends BaseEntity {
     private String description;
 
