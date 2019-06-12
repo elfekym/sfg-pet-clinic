b0c6af46b78ab0cb324dc2dd184ee7e354a5ee6f
Mohamed <mohamed.e@trufla.com>
2019-06-12 18:48:13 +0200

    Adjusted Owner and Pet to have functionality to get Pets and Builder. Added 4 end points for handling adding and update Pets Added tests for PetController, #56

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -10,11 +10,25 @@ import java.util.Set;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
+//@Builder
 @Entity
 @Table(name = "pets")
 public class Pet extends BaseEntity{
+
     private String name;
+
+    @Builder
+    public Pet(Long id, String name, PetType petType, Owner owner, LocalDate birthDate, Set<Visit> visits) {
+        super(id);
+        this.name = name;
+        this.petType = petType;
+        this.owner = owner;
+        this.birthDate = birthDate;
+        if (visits == null || visits.size() > 0 ) {
+            this.visits = visits;
+        }
+    }
+
     @ManyToOne
     @JoinColumn(name = "type_id")
     private PetType petType;
